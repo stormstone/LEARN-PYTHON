949919e7d949a0be720b1641151b72fa54cc9256
stonehell <2499144744@qq.com>
2017-10-07 18:11:53 +0800

    filter admin

--- imooc/Django_GettingStarted/blog/admin.py
+++ imooc/Django_GettingStarted/blog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from models import Article
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time',)
+
+
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
--- imooc/Django_GettingStarted/blog/models.py
+++ imooc/Django_GettingStarted/blog/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32, default='Title')
     content = models.TextField(null=True)
+    pub_time = models.DateTimeField(null=True)
 
     def __unicode__(self):
         return self.title
