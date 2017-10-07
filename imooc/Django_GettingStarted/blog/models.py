f21306aba5e897c9e9d2445b849573e978509410
stonehell <2499144744@qq.com>
2017-10-07 17:51:30 +0800

    imooc django getting started

---
+++ imooc/Django_GettingStarted/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.db import models
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=32, default='Title')
+    content = models.TextField(null=True)
+
+    def __unicode__(self):
+        return self.title
