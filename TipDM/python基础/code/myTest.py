b62efea48497c2bb5a832f15ada742f946ec51f8
stonehell <2499144744@qq.com>
2017-10-11 21:39:15 +0800

    mytest

---
+++ TipDM/python基础/code/myTest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017-10-11 21:35
+# @Author  : Storm
+# @Site    : 
+# @File    : myTest.py
+
+list = [1, 23, 4]
+list.append(5)
+print(list)
+
+list = [1, 45, 3]
+print(list)
+list2 = list.append(5)
+print(list.append(5))
+print(list2)
