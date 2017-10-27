d80be91440e8ffc3b94cd63911b0d315ab35ed6a
stonehell <2499144744@qq.com>
2017-10-27 22:03:38 +0800

    z_mytest

---
+++ TipDM/pythonDataMining/code/day09.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017-10-27 20:00
+# @Author  : Storm
+# @File    : day09.py
--- TipDM/pythonDataMining/code/z_mytest_createTrainData.py
+++ TipDM/pythonDataMining/code/z_mytest_createTrainData.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2017-10-24 16:55
+# @Time    : 2017-10-27 21:40
 # @Author  : Storm
-# @File    : mytest.py
-import random
+# @File    : z_mytest_createTrainData.py
 
-import pandas as pd
+import random
 from pandas import DataFrame
 import numpy as np
 
 n = 2000
 data = DataFrame(np.random.random((n, 8)), columns=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
-data.to_csv('./data/mytest.csv')
-data = pd.read_csv('./data/mytest.csv')
 a = np.random.random((n, 1))
 # o=a+10*sin(f*f)+random()*(c+d)
 for i in range(n):
     a[i] = data.loc[i, 'a'] + np.sin(data.loc[i, 'b'] + data.loc[i, 'f'] * data.loc[i, 'f']) * 10 + random.random() * (
         data.loc[i, 'c'] + data.loc[i, 'd'])
 data['输出'] = a
-data.to_csv('./data/mytest_train.csv')
+data.to_csv('./data/mytest_trainData.csv')
