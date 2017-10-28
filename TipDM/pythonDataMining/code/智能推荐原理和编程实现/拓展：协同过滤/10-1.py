98f26d241a5f5db31d0d245cbcfb8d718462ce0a
stonehell <2499144744@qq.com>
2017-10-28 16:37:48 +0800

    change error

--- TipDM/pythonDataMining/code/智能推荐原理和编程实现/拓展：协同过滤/10-1.py
+++ TipDM/pythonDataMining/code/智能推荐原理和编程实现/拓展：协同过滤/10-1.py
@@ -1,27 +1,73 @@
-#-*- coding: utf-8 -*-
-#使用基于UBCF算法对电影进行推荐
+# -*- coding: utf-8 -*-
+# 使用基于UBCF算法对电影进行推荐
 from __future__ import print_function
 import pandas as pd
-import os
-os.getcwd()
-os.chdir('E:\\网课孵化项目\\Python集训营第一届 2017.10.09~11.09\\智能推荐原理和编程实现')
+
+
+def prediction(df, userdf, Nn=15):  # Nn邻居个数
+    corr = df.T.corr();
+    rats = userdf.copy()
+    for usrid in userdf.index:
+        dfnull = df.loc[usrid][df.loc[usrid].isnull()]
+        usrv = df.loc[usrid].mean()  # 评价平均值
+        for i in range(len(dfnull)):
+            nft = (df[dfnull.index[i]]).notnull()
+            # 获取邻居列表
+            if (Nn <= len(nft)):
+                nlist = df[dfnull.index[i]][nft][:Nn]
+            else:
+                nlist = df[dfnull.index[i]][nft][:len(nft)]
+            nlist = nlist[corr.loc[usrid, nlist.index].notnull()]
+            nratsum = 0
+            corsum = 0
+            if (0 != nlist.size):
+                nv = df.loc[nlist.index, :].T.mean()  # 邻居评价平均值
+                for index in nlist.index:
+                    ncor = corr.loc[usrid, index]
+                    nratsum += ncor * (df[dfnull.index[i]][index] - nv[index])
+                    corsum += abs(ncor)
+                if (corsum != 0):
+                    rats.at[usrid, dfnull.index[i]] = usrv + nratsum / corsum
+                else:
+                    rats.at[usrid, dfnull.index[i]] = usrv
+            else:
+                rats.at[usrid, dfnull.index[i]] = None
+    return rats
+
+
+def recomm(df, userdf, Nn=15, TopN=3):
+    ratings = prediction(df, userdf, Nn)  # 获取预测评分
+    recomm = []  # 存放推荐结果
+    for usrid in userdf.index:
+        # 获取按NA值获取未评分项
+        ratft = userdf.loc[usrid].isnull()
+        ratnull = ratings.loc[usrid][ratft]
+        # 对预测评分进行排序
+        if (len(ratnull) >= TopN):
+            sortlist = (ratnull.sort_values(ascending=False)).index[:TopN]
+        else:
+            sortlist = ratnull.sort_values(ascending=False).index[:len(ratnull)]
+        recomm.append(sortlist)
+    return ratings, recomm
+
+
 ############    主程序   ##############
 if __name__ == "__main__":
     print("\n--------------使用基于UBCF算法对电影进行推荐 运行中... -----------\n")
-    traindata = pd.read_csv('u1.base',sep='\t', header=None,index_col=None)
-    testdata = pd.read_csv('u1.test',sep='\t', header=None,index_col=None)
-    #删除时间标签列
-    traindata.drop(3,axis=1, inplace=True)
-    testdata.drop(3,axis=1, inplace=True)
-    #行与列重新命名
-    traindata.rename(columns={0:'userid',1:'movid',2:'rat'}, inplace=True)
-    testdata.rename(columns={0:'userid',1:'movid',2:'rat'}, inplace=True)
-    traindf=traindata.pivot(index='userid', columns='movid', values='rat')
-    testdf=testdata.pivot(index='userid', columns='movid', values='rat')
-    traindf.rename(index={i:'usr%d'%(i) for i in traindf.index} , inplace=True)
-    traindf.rename(columns={i:'mov%d'%(i) for i in traindf.columns} , inplace=True)
-    testdf.rename(index={i:'usr%d'%(i) for i in testdf.index} , inplace=True)
-    testdf.rename(columns={i:'mov%d'%(i) for i in testdf.columns} , inplace=True)
-    userdf=traindf.loc[testdf.index]
-    #获取预测评分和推荐列表
-    trainrats,trainrecomm=recomm(traindf,userdf)
+    traindata = pd.read_csv('u1.base', sep='\t', header=None, index_col=None)
+    testdata = pd.read_csv('u1.test', sep='\t', header=None, index_col=None)
+    # 删除时间标签列
+    traindata.drop(3, axis=1, inplace=True)
+    testdata.drop(3, axis=1, inplace=True)
+    # 行与列重新命名
+    traindata.rename(columns={0: 'userid', 1: 'movid', 2: 'rat'}, inplace=True)
+    testdata.rename(columns={0: 'userid', 1: 'movid', 2: 'rat'}, inplace=True)
+    traindf = traindata.pivot(index='userid', columns='movid', values='rat')
+    testdf = testdata.pivot(index='userid', columns='movid', values='rat')
+    traindf.rename(index={i: 'usr%d' % (i) for i in traindf.index}, inplace=True)
+    traindf.rename(columns={i: 'mov%d' % (i) for i in traindf.columns}, inplace=True)
+    testdf.rename(index={i: 'usr%d' % (i) for i in testdf.index}, inplace=True)
+    testdf.rename(columns={i: 'mov%d' % (i) for i in testdf.columns}, inplace=True)
+    userdf = traindf.loc[testdf.index]
+    # 获取预测评分和推荐列表
+    trainrats, trainrecomm = recomm(traindf, userdf)
--- TipDM/pythonDataMining/code/智能推荐原理和编程实现/电影推荐.py
+++ TipDM/pythonDataMining/code/智能推荐原理和编程实现/电影推荐.py
@@ -5,121 +5,127 @@ Created on Fri Aug 18 10:50:07 2017
 @author: Administrator
 """
 
-import pandas as pd  
-import numpy as np  
-  
+import pandas as pd
+import numpy as np
+
 # 读取数据  
-data={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,  
- 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5},  
-'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,   
- 'Just My Luck': 1.5, 'The Night Listener': 3.0},   
-'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,  
- 'Superman Returns': 3.5, 'The Night Listener': 4.0},  
-'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,  
- 'The Night Listener': 4.5, 'You, Me and Dupree': 2.5},  
-'Mick LaSalle': {'Just My Luck': 2.0, 'Lady in the Water': 3.0,'Superman Returns': 3.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 2.0},   
-'Jack Matthews': {'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},  
-'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}  
-  
+data = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+                      'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5},
+        'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
+                         'Just My Luck': 1.5, 'The Night Listener': 3.0},
+        'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+                             'Superman Returns': 3.5, 'The Night Listener': 4.0},
+        'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+                         'The Night Listener': 4.5, 'You, Me and Dupree': 2.5},
+        'Mick LaSalle': {'Just My Luck': 2.0, 'Lady in the Water': 3.0, 'Superman Returns': 3.0,
+                         'The Night Listener': 3.0, 'You, Me and Dupree': 2.0},
+        'Jack Matthews': {'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0,
+                          'You, Me and Dupree': 3.5},
+        'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}}
+
 # 转换为数据框  
-data = pd.DataFrame(data)  
+data = pd.DataFrame(data)
 # 将数据中的空值nan变为0  
-data = data.fillna(0)  
+data = data.fillna(0)
 # 转置，每一列代表一个电影  
-mdata = data.T  
-  
+mdata = data.T
+
 # 计算不同电影的相似度，将数据规范化为[ 0，1 ]  
-corr = mdata.corr(method = 'pearson')
-corr = 0.5+corr*0.5 
-d1 = mdata.ix[:,0]
-d2 = mdata.ix[:,1]
-d1.corr(d2,method='pearson')
+corr = mdata.corr(method='pearson')
+corr = 0.5 + corr * 0.5
+d1 = mdata.ix[:, 0]
+d2 = mdata.ix[:, 1]
+d1.corr(d2, method='pearson')
 # 或者
-mcors = np.corrcoef(mdata, rowvar=0)  
-mcors = 0.5+mcors*0.5  
-mcors = pd.DataFrame(mcors, columns=mdata.columns, index=mdata.columns)  
-  
+mcors = np.corrcoef(mdata, rowvar=0)
+mcors = 0.5 + mcors * 0.5
+mcors = pd.DataFrame(mcors, columns=mdata.columns, index=mdata.columns)
+
+
 # 计算每个用户的每个电影的分数 ：为没有用户评分的电影用相似度估算
 # matrix:用户-电影矩阵  
 # mcors:电影相关矩阵 
 # score:得分   
-def cal_score(matrix,mcors,item,user):  
-    totscore = 0  
-    totsims = 0  
-    score = 0  
-    if pd.isnull(matrix[item][user]) or matrix[item][user]==0:  
-        for mitem in matrix.columns:  
-            if matrix[mitem][user]==0:  
-                continue  
-            else:  
-                totscore += matrix[mitem][user]*mcors[item][mitem]  
-                totsims += mcors[item][mitem]  
-        score = totscore/totsims  
-    else:  
-        score = matrix[item][user]  
-    return score  
-  
-cal_score(mdata,mc,'Just My Luck','Jack Matthews')
-cal_score(mdata,mc,'Just My Luck','Toby')
+def cal_score(matrix, mcors, item, user):
+    totscore = 0
+    totsims = 0
+    score = 0
+    if pd.isnull(matrix[item][user]) or matrix[item][user] == 0:
+        for mitem in matrix.columns:
+            if matrix[mitem][user] == 0:
+                continue
+            else:
+                totscore += matrix[mitem][user] * mcors[item][mitem]
+                totsims += mcors[item][mitem]
+        score = totscore / totsims
+    else:
+        score = matrix[item][user]
+    return score
 
-# 计算得分矩阵   
+
+cal_score(mdata, mcors, 'Just My Luck', 'Jack Matthews')
+cal_score(mdata, mcors, 'Just My Luck', 'Toby')
+
+
+# 计算得分矩阵
 # score_matrix:不同用户的电影得分矩阵
-def cal_matscore(matrix,mcors):  
-    score_matrix = pd.DataFrame(np.zeros(matrix.shape) , columns=matrix.columns, index=matrix.index)  
-    for mitem in score_matrix.columns:  
-        for muser in score_matrix.index:  
-            score_matrix[mitem][muser]  = cal_score(matrix,mcors,mitem,muser)  
-    return score_matrix  
-  
+def cal_matscore(matrix, mcors):
+    score_matrix = pd.DataFrame(np.zeros(matrix.shape), columns=matrix.columns, index=matrix.index)
+    for mitem in score_matrix.columns:
+        for muser in score_matrix.index:
+            score_matrix[mitem][muser] = cal_score(matrix, mcors, mitem, muser)
+    return score_matrix
+
+
 # give recommendations: depending on the score matrix  
 # matrix: 矩阵:用户-电影矩阵
 # score_matrix:不同用户的电影得分矩阵   
 # n: 推荐系数
-def recommend(matrix,score_matrix,user,n):  
-    user_ratings = matrix.ix[user]  
-    not_rated_item = user_ratings[user_ratings==0]  
+def recommend(matrix, score_matrix, user, n):
+    user_ratings = matrix.ix[user]
+    not_rated_item = user_ratings[user_ratings == 0]
     recom_items = {}
-    for item in not_rated_item.index:  
-        recom_items[item] = score_matrix[item][user]  
+    for item in not_rated_item.index:
+        recom_items[item] = score_matrix[item][user]
     recom_items = pd.Series(recom_items)
-    recom_items = recom_items.sort_values(ascending=False)  
-    return recom_items[:n]    
+    recom_items = recom_items.sort_values(ascending=False)
+    return recom_items[:n]
 
-#main  
-score_matrix = cal_matscore(mdata,mcors)  
-for i in range(10):  
-    user = input(str(i)+'please input the name of user:')  
-    print(recommend(mdata,score_matrix,user,2))
-   
+
+# main
+score_matrix = cal_matscore(mdata, mcors)
+for i in range(10):
+    user = input(str(i) + 'please input the name of user:')
+    print(recommend(mdata, score_matrix, user, 2))
+
 #### 检验
 user = 'Toby'
-    user_ratings = mdata.ix[user]  
-    not_rated_item = user_ratings[user_ratings==0]  
-    recom_items = {}  
-    for item in not_rated_item.index:  
-        recom_items[item] = score_matrix[item][user]  
-    recom_items = pd.Series(recom_items)  
-    recom_items = recom_items.sort_values(ascending=False) 
+user_ratings = mdata.ix[user]
+not_rated_item = user_ratings[user_ratings == 0]
+recom_items = {}
+for item in not_rated_item.index:
+    recom_items[item] = score_matrix[item][user]
+recom_items = pd.Series(recom_items)
+recom_items = recom_items.sort_values(ascending=False)
 
 score_matrix = np.zeros(mdata.shape)
-score_matrix = pd.DataFrame(score_matrix, columns=mdata.columns, index=mdata.index)  
-    for mitem in score_matrix.columns:  
-        for muser in score_matrix.index:  
-            score_matrix[mitem][muser]  = cal_score(mdata,mcors,mitem,muser)   
+score_matrix = pd.DataFrame(score_matrix, columns=mdata.columns, index=mdata.index)
+for mitem in score_matrix.columns:
+    for muser in score_matrix.index:
+        score_matrix[mitem][muser] = cal_score(mdata, mcors, mitem, muser)
 
-
-    totscore = 0  
-    totsims = 0  
-    score = 0  
-    if pd.isnull(mdata[item][user]) or mdata[item][user]==0:  
-        for mitem in mdata.columns:  
-            if mdata[mitem][user]==0:  
-                continue  
-            else:  
-                totscore += mdata[mitem][user]*mcors[item][mitem]  
-                totsims += mcors[item][mitem]  
-        score = totscore/totsims  
-    else:  
-        score = mdata[item][user]  
+totscore = 0
+totsims = 0
+score = 0
+if pd.isnull(mdata[item][user]) or mdata[item][user] == 0:
+    for mitem in mdata.columns:
+        if mdata[mitem][user] == 0:
+            continue
+        else:
+            totscore += mdata[mitem][user] * mcors[item][mitem]
+            totsims += mcors[item][mitem]
+    score = totscore / totsims
+else:
+    score = mdata[item][user]
 item = 'Superman Returns'
 mitem = 'You, Me and Dupree'
