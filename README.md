7cdaff982bfaaba5e7dde6fb94caaac30bbb1f3f
stonehell <2499144744@qq.com>
2017-11-24 19:36:14 +0800

    readme.md 完善链接

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # LEARN-PYTHON
+
+学习python的一些东西。
+
+  cins-bigdata[https://github.com/stormstone/LEARN-PYTHON/tree/master/cins-bigdata] ：学院大数据月比赛，银行离队率预测。
+
+  FlaskWeb[https://github.com/stormstone/LEARN-PYTHON/tree/master/FlaskWeb] ：学习flask框架内容。
+
+  imooc[https://github.com/stormstone/LEARN-PYTHON/tree/master/imooc] ：imooc网视频学习内容。
+
+  mayday_lyric_analyze[https://github.com/stormstone/LEARN-PYTHON/tree/master/mayday_lyric_analyze] ：五月天100首歌曲词频统计。
+
+  MINE-PY27[https://github.com/stormstone/LEARN-PYTHON/tree/master/MINE-PY27] ：python2.7相关。
+  
+  MINE-PY36[https://github.com/stormstone/LEARN-PYTHON/tree/master/MINE-PY36] ：python3.6相关。
+  
+  spider_competition_info[https://github.com/stormstone/LEARN-PYTHON/tree/master/spider_competition_info] ：赛氪网站比赛信息爬取。
+  
+  
+  TipDM[https://github.com/stormstone/LEARN-PYTHON/tree/master/TipDM] ：泰迪第一届python培训相关。
---
+++ cins-bigdata/哦豁-石华-201531060570.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+# @Time    : 2017-11-05 19:59
+# @Author  : Storm
+# @File    : a_third.py
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+dataTrain = pd.read_csv('./data/train.csv', header=-1)  # 读取训练集
+trainX = dataTrain.iloc[:, 0:8].as_matrix()
+trainY = dataTrain.iloc[:, 8].as_matrix()
+
+# trainX_denoising = dataTrain.iloc[:1999, 0:8].as_matrix()
+trainX_denoising = dataTrain.iloc[:599, 4:7].as_matrix()
+trainY_denoising = dataTrain.iloc[:599, 8].as_matrix()
+
+dataTest = pd.read_csv('./data/test.csv', header=-1)  # 读取测试集
+dataTest_lowd = dataTest.iloc[:, 4:7].as_matrix()
+
+lr = LinearRegression()  # 建立LR模型
+lr.fit(trainX_denoising, trainY_denoising)
+res_lr = lr.predict(dataTest_lowd)
+print('线性回归预测结果:', res_lr)
+
+print('===================================================')
+print('线性回归参数：', lr.coef_)
+dataResult = pd.DataFrame(res_lr)
+# print(dataResult)
+dataResult.to_csv('./data/my_answer_third.csv', header=None, index=None)
